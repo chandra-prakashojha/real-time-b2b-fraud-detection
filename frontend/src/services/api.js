0f520f10a38629aa29048889dd9722449f1cab54
chandra prakash <shanuojha08@gmail.com>
2026-07-17 18:10:18 +0000

    deployment things

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://organic-zebra-pj59jv66r4gqf9wjp-5000.app.github.dev/api",
+  baseURL: "/api",
 });
 
 // Automatically attach JWT token
